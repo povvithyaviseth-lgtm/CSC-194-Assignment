551abacccdf5dec162db51a1184be4acbe9b68a3
povvithya viseth <povvithyaviseth@gmail.com>
2026-09-07 23:29:10 -0700

    feat: When User Clicked "Moo", It Upper Case all the Letter and Added "-MOO" at the End of the Sentence

--- fancifymytext.js
+++ fancifymytext.js
@@ -23,6 +23,20 @@ function radioButton()
 
 function mooButton()
 {
-    let userInput = document.getElementById("textArea").value
-    console.log(userInput)
+    let userInput = document.getElementById("textArea").value;
+    userInput = userInput.toUpperCase();
+
+    let sentence = userInput.split(".");
+
+    for(let index = 0; index < sentence.length; index++)
+    {
+        if(sentence[index].trim() == "")
+            continue;
+        
+        let tokens = sentence[index].trim().split(" ");
+        tokens[tokens.length - 1] += "-MOO";
+        sentence[index] = tokens.join(" ");
+    }
+
+    document.getElementById("textArea").value = sentence.join(".");
 }
